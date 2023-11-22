ab4b647ee780a2de914376975bf8565fe956c5fe
Juan Paulo Fricke <89048734+JuanFricke@users.noreply.github.com>
2023-11-22 19:04:42 +0000

    losing my mind with this dumb js code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'refresh_data', to: 'pages#refresh_data'
-  get 'home', to: 'pages#home'
+  get 'test', to: 'pages#test'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "pages#index"
+  root "pages#home"
 end
