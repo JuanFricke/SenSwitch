eb3228b15dcdfa6be828ece898234c8739f0956f
Juan Paulo Fricke <89048734+JuanFricke@users.noreply.github.com>
2023-12-02 21:10:59 +0000

    ajustes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'refresh_data', to: 'pages#refresh_data'
   get 'test', to: 'pages#test'
+  get 'sobre', to: 'pages#sobre'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
