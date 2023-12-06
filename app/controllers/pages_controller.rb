b44c9573630416bb233c09b6fdc83bae5555aa3a
Juan Paulo Fricke <89048734+JuanFricke@users.noreply.github.com>
2023-12-06 06:05:47 +0000

    monitor kinda working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,12 +12,53 @@ class PagesController < ApplicationController
   end
 
   def refresh_data
+    # Leitura do arquivo JSON original
     json_data = read_json_file('combined_data.json')
-    render json: {
+
+    # Processamento e conversão do JSON
+    processed_data = {
       bd_update_time: json_data['timestamp']['timestamp'],
-      estacoes_metereologicas: json_data['estacoes_data'],
-      microparticulas: json_data['microparticulas_data']
+      estacoes_metereologicas: process_estacoes(json_data['estacoes_data']),
+      microparticulas: process_microparticulas(json_data['microparticulas_data'])
     }
+
+    # Renderização do JSON processado
+    render json: processed_data
+  end
+
+  def process_estacoes(estacoes_data)
+    estacoes_data.map do |estacao|
+      {
+        deviceName: estacao['deviceInfo']['deviceName'],
+        temperature: estacao['object']['internal_sensors'][0]['v'].to_f,
+        humidity: estacao['object']['internal_sensors'][1]['v'].to_f,
+        rain_lvl: estacao['object']['modules'][0]['v'].to_f,
+        avg_wind_speed: estacao['object']['modules'][1]['v'].to_f,
+        gust_wind_speed: estacao['object']['modules'][2]['v'].to_f,
+        wind_direction: estacao['object']['modules'][3]['v'].to_i,
+        luminosity: estacao['object']['modules'][6]['v'].to_i,
+        uv: estacao['object']['modules'][7]['v'].to_f,
+        solar_radiation: estacao['object']['modules'][8]['v'].to_f,
+        atm_pres: estacao['object']['modules'][9]['v'].to_f
+      }
+    end
+  end
+
+  def process_microparticulas(microparticulas_data)
+    microparticulas_data.map do |microparticulas|
+      {
+        deviceName: microparticulas['deviceName'],
+        co2: microparticulas['object']['co2'],
+        humidity: microparticulas['object']['humidity'].to_f,
+        nh3: microparticulas['object']['nh3'],
+        noise: microparticulas['object']['noise'].to_f,
+        pm10: microparticulas['object']['pm10'],
+        pm1_0: microparticulas['object']['pm1_0'],
+        pm2_5: microparticulas['object']['pm2_5'],
+        temperature: microparticulas['object']['temperature'].to_f,
+        voltage: microparticulas['object']['voltage'].to_f
+      }
+    end
   end
 
   private
---
+++ app/javascript/monitor.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+    // Função para criar coluna com base nos dados do sensor
+    function createSensorColumn(sensorData) {
+      var columnHTML = `
+        <div class="column is-one-third">
+          <div class="box" id="${sensorData.deviceName}">
+            <div class="notification">
+              <h3>${sensorData.deviceName}</h3>
+              Adicione suas mensagens ou dados específicos aqui.
+            </div>
+            <div class="sensor-menu" id="${sensorData.deviceName}-menu" style="display: none;">
+              <div class="notification">
+                <h3>Informações do Sensor</h3>
+                ${createTable(sensorData)}
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+      return columnHTML;
+    }
+  
+    function createTable(sensorData) {
+      var tableHTML = '<table class="table is-bordered is-striped is-fullwidth"><tbody>';
+  
+      var labels = {
+        'deviceName': 'Nome do Dispositivo',
+        'temperature': 'Temperatura',
+        'humidity': 'Umidade',
+        'rain_lvl': 'Nível de Chuva',
+        'avg_wind_speed': 'Velocidade Média do Vento',
+        'gust_wind_speed': 'Velocidade Máxima do Vento',
+        'wind_direction': 'Direção do Vento',
+        'luminosity': 'Luminosidade',
+        'uv': 'Índice UV',
+        'solar_radiation': 'Radiação Solar',
+        'atm_pres': 'Pressão Atmosférica',
+        'co2': 'Dióxido de Carbono (CO2)',
+        'nh3': 'Amônia (NH3)',
+        'noise': 'Nível de Ruído',
+        'pm10': 'Partículas PM10',
+        'pm1_0': 'Partículas PM1.0',
+        'pm2_5': 'Partículas PM2.5',
+        'voltage': 'Voltagem',
+        // Adicione mais rótulos conforme necessário
+      };
+  
+      for (var key in sensorData) {
+        tableHTML += '<tr>';
+        tableHTML += `<th>${labels[key]}</th>`;
+        tableHTML += `<td>${sensorData[key]}</td>`;
+        tableHTML += '</tr>';
+      }
+  
+      tableHTML += '</tbody></table>';
+      return tableHTML;
+    }
+  
+    // Função para carregar e processar os dados
+    function loadData() {
+      $.getJSON('/refresh_data', function (jsonData) {
+        // Limpa as colunas existentes antes de adicionar novas
+        $("#sensorColumns").empty();
+  
+        // Adiciona colunas com base nos dados do JSON
+        jsonData.estacoes_metereologicas.forEach(function (sensorData) {
+          var columnHTML = createSensorColumn(sensorData);
+          $("#sensorColumns").append(columnHTML);
+        });
+  
+        // Adiciona colunas para micropartículas, se houver
+        if (jsonData.microparticulas) {
+          jsonData.microparticulas.forEach(function (sensorData) {
+            var columnHTML = createSensorColumn(sensorData);
+            $("#sensorColumns").append(columnHTML);
+          });
+        }
+  
+        // Evento de clique para exibir/ocultar informações do sensor
+        $(".notification").click(function () {
+          var sensorId = $(this).closest(".box").attr("id");
+          var sensorMenu = $("#" + sensorId + "-menu .sensor-menu");
+  
+          if (sensorMenu.is(":visible")) {
+            sensorMenu.slideUp();
+          } else {
+            $(".sensor-menu").not(sensorMenu).slideUp();
+            sensorMenu.slideDown();
+  
+            // Atualiza o conteúdo da área de informações do sensor
+            var infoContent = $("#" + sensorId + "-menu .notification").html();
+            var sensorInfoContent = createTable(jsonData.estacoes_metereologicas.find(sensor => sensor.deviceName === sensorId) || jsonData.microparticulas.find(sensor => sensor.deviceName === sensorId));
+            $("#infoPanel").html(
+              `<div class="message">
+                <h3 class="message-header">Informações do Sensor (${sensorId})</h3>
+                <div class="message-body">
+                  <div class ="table is-bordered is-striped is-fullwidth">
+                    ${sensorInfoContent}
+                  </div>
+                </div>
+              </div>`
+            );
+          }
+        });
+      });
+    }
+  
+    // Chama a função de carga de dados inicial
+    loadData();
+  
+    // Define um intervalo de atualização (a cada 5 minutos neste exemplo)
+    setInterval(loadData, 5 * 60 * 1000);
+  });
+  
