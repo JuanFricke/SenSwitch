2076563283a8b97af43990739a5e9cf4fc59c16c
Juan Paulo Fricke <89048734+JuanFricke@users.noreply.github.com>
2023-12-06 04:36:52 +0000

    Initial tile based interface

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'refresh_data', to: 'pages#refresh_data'
   get 'test', to: 'pages#test'
   get 'sobre', to: 'pages#sobre'
+  get 'monitor', to: 'pages#monitor'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
